0bfa7e24ed053e63774995852faf570e7f2618ab
Casey Marshall <cmars@cmarstech.com>
2014-04-26 17:06:43 -0600

    Idempotent Get/CreateMachine from image.

--- main.go
+++ main.go
@@ -5,35 +5,50 @@ import (
 	"log"
 	"os"
 
-	"github.com/riobard/go-virtualbox"
+	vbox "github.com/riobard/go-virtualbox"
 )
 
 func die(err error) {
 	log.Fatalln(err)
 }
 
-func main() {
-	if len(os.Args) < 2 {
-		die(fmt.Errorf("Usage: <img file>"))
-	}
-	imgfile := os.Args[1]
-	m, err := virtualbox.CreateMachine("foo", "")
+func MachineWithImage(name, imgfile string) (*vbox.Machine, error) {
+	m, err := vbox.CreateMachine(name, "")
 	if err != nil {
-		die(err)
+		return nil, err
 	}
-	err = m.AddStorageCtl("storecntl", virtualbox.StorageController{
-		SysBus:      virtualbox.SysBusSATA,
+	err = m.AddStorageCtl("storecntl", vbox.StorageController{
+		SysBus:      vbox.SysBusSATA,
 		Ports:       1,
-		Chipset:     virtualbox.CtrlIntelAHCI,
+		Chipset:     vbox.CtrlIntelAHCI,
 		HostIOCache: true,
 		Bootable:    true,
 	})
 	if err != nil {
-		die(err)
+		return nil, err
 	}
-	err = m.AttachStorage("storecntl", virtualbox.StorageMedium{
-		DriveType: virtualbox.DriveHDD,
+	err = m.AttachStorage("storecntl", vbox.StorageMedium{
+		DriveType: vbox.DriveHDD,
 		Medium:    imgfile,
 	})
+	if err != nil {
+		return nil, err
+	}
+	return m, nil
+}
+
+func main() {
+	if len(os.Args) < 3 {
+		die(fmt.Errorf("Usage: <name> <imgfile>"))
+	}
+	name := os.Args[1]
+	imgfile := os.Args[2]
+	m, err := vbox.GetMachine(name)
+	if err == vbox.ErrMachineNotExist {
+		m, err = MachineWithImage(name, imgfile)
+	}
+	if err != nil {
+		die(err)
+	}
 	fmt.Println(m)
 }
