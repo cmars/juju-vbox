2e741705ebdf87f42106d4b62ec01e6fa1023615
Casey Marshall <cmars@cmarstech.com>
2014-04-27 17:43:25 -0600

    Idempotent networking setup, other minor fixes.
    The host only networking interface name has a prefix appended to the key
    that comes back from Machine.HostonlyNets & virtualbox.DHCPs. Using
    these to match the current configured networking entities for
    idempotency.
    
    Hard-linking the image file because Virtualbox likes to delete it along
    with the virtual machine. This is usually not what you want for a cached
    cloud image.
    
    Starting the machine after creating/matching it.
    
    Conflicts:
    	main.go

--- main.go
+++ main.go
@@ -5,7 +5,10 @@ import (
 	"log"
 	"net"
 	"os"
+	"path"
+	"time"
 
+	"code.google.com/p/go-uuid/uuid"
 	vbox "github.com/riobard/go-virtualbox"
 )
 
@@ -13,12 +16,80 @@ func die(err error) {
 	log.Fatalln(err)
 }
 
+const (
+	storageCtrlName = "storctrl0"
+	ifac0Name       = "vboxnet0"
+)
+
+func CreateFirstIFac(m *vbox.Machine) error {
+	ifacName := ifac0Name // TODO: configurable?
+	ifacKey := fmt.Sprintf("HostInterfaceNetworking-%s", ifacName)
+	honets, err := vbox.HostonlyNets()
+	if err != nil {
+		return err
+	}
+	log.Println("Host Only Nets:", honets)
+	hon, ok := honets[ifacKey]
+	if !ok {
+		hon, err = vbox.CreateHostonlyNet()
+		if err != nil {
+			return err
+		}
+		ifacName = hon.Name
+		hon.IPv4 = net.IPNet{
+			IP:   net.IPv4(192, 168, 50, 1),
+			Mask: net.IPv4Mask(255, 255, 255, 0),
+		}
+		err = hon.Config()
+		if err != nil {
+			return err
+		}
+		err = m.SetNIC(1, vbox.NIC{
+			Network:         vbox.NICNetHostonly,
+			Hardware:        vbox.IntelPro1000MTDesktop,
+			HostonlyAdapter: hon.Name,
+		})
+		if err != nil {
+			return err
+		}
+	}
+	dhcps, err := vbox.DHCPs()
+	if err != nil {
+		return err
+	}
+	log.Println("DHCP Servers", dhcps)
+	dhcp, ok := dhcps[ifacKey]
+	if !ok {
+		dhcp = &vbox.DHCP{
+			NetworkName: ifacName,
+			IPv4: net.IPNet{
+				IP:   hon.IPv4.IP,
+				Mask: hon.IPv4.Mask,
+			},
+			LowerIP: net.IPv4(192, 168, 50, 2),
+			UpperIP: net.IPv4(192, 168, 50, 250),
+		}
+		dhcp.Enabled = true // Make sure it's turned on.
+		err = vbox.AddHostonlyDHCP(ifacName, *dhcp)
+	}
+	return err
+}
+
 func CreateMachineWithImage(name, imgfile string) (*vbox.Machine, error) {
+	// Create a hard link to the image file. Virtualbox likes to delete the
+	// image with the machine.
+	imglink := path.Join(path.Dir(imgfile),
+		fmt.Sprintf(".%s.%s", path.Base(imgfile), uuid.New()))
+	err := os.Link(imgfile, imglink)
+	if err != nil {
+		return nil, err
+	}
+
 	m, err := vbox.CreateMachine(name, "")
 	if err != nil {
 		return nil, err
 	}
-	err = m.AddStorageCtl("storecntl", vbox.StorageController{
+	err = m.AddStorageCtl(storageCtrlName, vbox.StorageController{
 		SysBus:      vbox.SysBusSATA,
 		Ports:       1,
 		Chipset:     vbox.CtrlIntelAHCI,
@@ -28,38 +99,16 @@ func CreateMachineWithImage(name, imgfile string) (*vbox.Machine, error) {
 	if err != nil {
 		return nil, err
 	}
-	err = m.AttachStorage("storecntl", vbox.StorageMedium{
+	err = m.AttachStorage(storageCtrlName, vbox.StorageMedium{
 		DriveType: vbox.DriveHDD,
-		Medium:    imgfile,
+		Medium:    imglink,
 	})
 	if err != nil {
 		return nil, err
 	}
-	honets, err := vbox.HostonlyNets()
-	if err != nil {
-		return nil, err
-	}
-	hon, ok := honets["vboxnet0"]
-	if !ok {
-		hon, err = vbox.CreateHostonlyNet()
-		if err != nil {
-			return nil, err
-		}
-		hon.Name = "vboxnet0"
-		hon.IPv4 = net.IPNet{
-			IP:   net.IPv4(192, 168, 50, 1),
-			Mask: net.IPv4Mask(255, 255, 255, 0),
-		}
-		//hon.DHCP = true
-		err = hon.Config()
-		if err != nil {
-			return nil, err
-		}
-	}
-	err = m.SetNIC(1, vbox.NIC{
-		Network:         vbox.NICNetHostonly,
-		Hardware:        vbox.IntelPro1000MTDesktop,
-		HostonlyAdapter: hon.Name,
+	err = m.SetNIC(2, vbox.NIC{
+		Network:  vbox.NICNetNAT,
+		Hardware: vbox.IntelPro1000MTDesktop,
 	})
 	if err != nil {
 		return nil, err
@@ -88,17 +137,17 @@ func main() {
 	if err != nil {
 		die(err)
 	}
-	/*
-		err = m.AddNATPF(1, "ssh", vbox.PFRule{
-			Proto:     vbox.PFTCP,
-			HostIP:    nil,
-			HostPort:  2222,
-			GuestIP:   nil,
-			GuestPort: 22,
-		})
+	err = m.Start()
+	if err != nil {
+		die(err)
+	}
+	for {
+		err = m.Refresh()
 		if err != nil {
-			die(err)
+			fmt.Println("refresh:", err)
+		} else {
+			fmt.Println(m)
 		}
-	*/
-	fmt.Println(m)
+		time.Sleep(5 * time.Second)
+	}
 }
