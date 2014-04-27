6e73833dd75c4287609ea91b41a9e93f52183ccb
Casey Marshall <cmars@cmarstech.com>
2014-04-26 18:17:35 -0600

    Setting up host-only and NAT adapters.

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"net"
 	"os"
 
 	vbox "github.com/riobard/go-virtualbox"
@@ -34,7 +35,36 @@ func CreateMachineWithImage(name, imgfile string) (*vbox.Machine, error) {
 	if err != nil {
 		return nil, err
 	}
+	honets, err := vbox.HostonlyNets()
+	if err != nil {
+		return nil, err
+	}
+	hon, ok := honets["vboxnet0"]
+	if !ok {
+		hon, err = vbox.CreateHostonlyNet()
+		if err != nil {
+			return nil, err
+		}
+		hon.Name = "vboxnet0"
+		hon.IPv4 = net.IPNet{
+			IP:   net.IPv4(192, 168, 50, 1),
+			Mask: net.IPv4Mask(255, 255, 255, 0),
+		}
+		//hon.DHCP = true
+		err = hon.Config()
+		if err != nil {
+			return nil, err
+		}
+	}
 	err = m.SetNIC(1, vbox.NIC{
+		Network:         vbox.NICNetHostonly,
+		Hardware:        vbox.IntelPro1000MTDesktop,
+		HostonlyAdapter: hon.Name,
+	})
+	if err != nil {
+		return nil, err
+	}
+	err = m.SetNIC(2, vbox.NIC{
 		Network:  vbox.NICNetNAT,
 		Hardware: vbox.IntelPro1000MTDesktop,
 	})
@@ -45,7 +75,7 @@ func CreateMachineWithImage(name, imgfile string) (*vbox.Machine, error) {
 }
 
 func main() {
-	//vbox.Verbose = true
+	vbox.Verbose = true
 	if len(os.Args) < 3 {
 		die(fmt.Errorf("Usage: <name> <imgfile>"))
 	}
@@ -58,15 +88,17 @@ func main() {
 	if err != nil {
 		die(err)
 	}
-	err = m.AddNATPF(1, "ssh", vbox.PFRule{
-		Proto:     vbox.PFTCP,
-		HostIP:    nil,
-		HostPort:  2222,
-		GuestIP:   nil,
-		GuestPort: 22,
-	})
-	if err != nil {
-		die(err)
-	}
+	/*
+		err = m.AddNATPF(1, "ssh", vbox.PFRule{
+			Proto:     vbox.PFTCP,
+			HostIP:    nil,
+			HostPort:  2222,
+			GuestIP:   nil,
+			GuestPort: 22,
+		})
+		if err != nil {
+			die(err)
+		}
+	*/
 	fmt.Println(m)
 }
